c2af18924204d526e4fa52f5376fc5bb0d4a5842
David-hosting <67858186+David-hosting@users.noreply.github.com>
2023-04-26 20:03:59 +0300

    Temp file creation
    A temp file is created for understanding how github works

---
+++ coffe.py
@@ -0,0 +1,1 @@
+print('Coffe=Woman')
--- nobitchesexception.py
+++ nobitchesexception.py
@@ -3,3 +3,4 @@ class NoBitchesException(Exception):
     def __init__(self, message : str) -> None:
         self.message = message
         super().__init__(self.message)
+    
