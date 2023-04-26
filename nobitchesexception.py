f512444df7d757309e5d2d5febafd45678f4711b
David-hosting <david.i.businesses@gmail.com>
2023-04-26 18:20:23 +0300

    Create a nobitches.py
    This is the first class of the project. This class raises a custom exception when "No bitches were found".
    
    Keep in mind that this project is only for entertainment and should not be taken seriously.

---
+++ nobitchesexception.py
@@ -0,0 +1,5 @@
+class NoBitchesException(Exception):
+    """This class is used to raise custom exceptions when no bitches are found."""
+    def __init__(self, message : str) -> None:
+        self.message = message
+        super().__init__(self.message)
